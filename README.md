6edcc427747c7734188d5b6946a4f72035f217a5
Roman Shamin <al4emist.artway@gmail.com>
2017-07-13 16:17:29 +0300

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# unsymbol-me-sketch
-Convert master symbols to artboards and detach symbol instances
+# Unsymbol Me
+
+<img width="256" height="256" src="Unsymbol Me.sketchplugin/Contents/Resources/Icon.png" title="Unsymbol Me Icon">
+
+<img width="192" height="30" src="https://osom.coffee/files/002/419/551/2419551/original/plugin-updates_2x.png" title="Plugin Update Badge">
+
+**Unsymbol Me 1.0 supports Sketch 45 plugin updating system**
+
+Unsymbol Me is a Sketch plugin that ‘unsymbols’ selected symbols — converts master symbols to artboards and detaches symbol instances.
+
+### Install
+
+1. Download and unzip: [unsymbol-me-sketch-master.zip].
+2. Double click `Unsymbol Me.sketchplugin`.
+
+[unsymbol-me-sketch-master.zip]: https://github.com/romashamin/unsymbol-me-sketch/archive/master.zip
+
+### System Requirements
+
+Unsymbol Me has been tested on Sketch 45.2 on macOS Sierra. If you have any problems, drop me a line: [@romanshamin].
+
+[@romanshamin]: https://twitter.com/romanshamin
+
+### Satisfied Pro?
+
+If you’re a professional web designer or a developer and Unsymbol Me saves your time, buy me an espresso to say ‘thanks’: [pay $3 by PayPal].
+
+[pay $3 by PayPal]: https://www.paypal.me/romanshamin/3
+
+### Thanks
+
+<a href="https://evilmartians.com/?utm_source=compo">
+<img src="https://evilmartians.com/badges/sponsored-by-evil-martians.svg" alt="Sponsored by Evil Martians" width="236" height="54"></a>
---
+++ Unsymbol Me.sketchplugin/Contents/Sketch/Unsymbol Me.js
@@ -0,0 +1,134 @@
+/**
+ * Unsymbol Me
+ *
+ * Copyright © 2017 Roman Shamin https://github.com/romashamin
+ * and licenced under the MIT licence. All rights not explicitly
+ * granted in the MIT license are reserved. See the included
+ * LICENSE file for more details.
+ *
+ * https://github.com/romashamin/
+ * https://twitter.com/romanshamin
+ */
+
+
+/*
+ * @param {Object} symbolsData
+ * @returns {string}
+ */
+
+function getMessageFromArgs(symbolsData) {
+  let resultMessage = '';
+  let masterPart    = '';
+  let verb          = '';
+  let instancePart  = '';
+
+  log(symbolsData);
+
+  // No master symbols, no instances
+  if (symbolsData.masters.count === 0 && symbolsData.instances.count === 0) {
+    resultMessage = 'select at least one symbol';
+  }
+
+  // If one or more masters
+  if (symbolsData.masters.count === 1) {
+    masterPart = `‘${symbolsData.masters.names[0]}’ symbol`;
+  } else {
+    masterPart = `${symbolsData.masters.count} symbols`;
+  }
+
+  // If one or more instances
+  if (symbolsData.instances.count === 1) {
+    const maybeName = symbolsData.instances.names[0];
+
+    if (maybeName) {
+      instancePart = `‘${maybeName}’ instance`;
+    } else {
+      instancePart = `1 instance`;
+    }
+  } else {
+    instancePart = `${symbolsData.instances.count} instances`;
+  }
+
+  // One master + one instance?
+  if (symbolsData.masters.count + symbolsData.instances.count === 1) {
+    verb = 'has';
+  } else {
+    verb = 'have';
+  }
+
+  // No masters + some instances?
+  if (symbolsData.masters.count === 0 && symbolsData.instances.count > 0) {
+    resultMessage = `${instancePart} ${verb} been affected`;
+  } else {
+    resultMessage = `${masterPart} and ${instancePart} ${verb} been affected`;
+  }
+
+  return resultMessage;
+}
+
+
+
+/*
+ * @param {NSArray} selection
+ * @returns {string}
+ */
+
+function unsymbolAll(selection) {
+  let symbolsData = {
+    masters   : {
+      names : [],
+      count : 0
+    },
+    instances : {
+      names : [],
+      count : 0
+    }
+  };
+
+  let loopSelection = selection.objectEnumerator();
+
+  while (object = loopSelection.nextObject()) {
+    if (object.isMemberOfClass(MSSymbolMaster.class())) {
+      const masterSymbol = object;
+
+      symbolsData.masters.names.push(masterSymbol.name());
+      symbolsData.masters.count += 1;
+
+      const affectedInstances = masterSymbol.allInstances().count();
+      symbolsData.instances.count += affectedInstances;
+
+      MSSymbolMaster.convertSymbolToArtboard(masterSymbol);
+    } else if (object.isMemberOfClass(MSSymbolInstance.class())) {
+      const instanceSymbol = object;
+
+      symbolsData.instances.names.push(instanceSymbol.name());
+      symbolsData.instances.count += 1;
+
+      instanceSymbol.detachByReplacingWithGroup();
+    }
+  }
+
+  return getMessageFromArgs(symbolsData);
+}
+
+
+
+/*
+ * @param {NSDictionary} context
+ */
+
+function onRun(context) {
+  const document   = context.document;
+  const selection  = context.selection;
+  const pluginName = 'Unsymbol Me';
+
+  if (selection.count() > 0) {
+    const resultMessage = unsymbolAll(selection);
+
+    document.showMessage(`${pluginName}: ${resultMessage}`);
+  } else {
+    document.showMessage(`${pluginName}: select at least one symbol`);
+  }
+}
+
+//onRun(context);
